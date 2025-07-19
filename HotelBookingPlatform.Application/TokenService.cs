9658aa7ce1d63bbdd0138bb2d1cd1d42485b114f
shahdsolliman <42022174@hti.edu.eg>
2025-07-19 03:44:38 +0300

    🔐 Update TokenService to read JWT config from appsettings

--- HotelBookingPlatform.Application/TokenService.cs
+++ HotelBookingPlatform.Application/TokenService.cs
@@ -1,7 +1,8 @@
-﻿using HotelBookingPlatform.Core.Entities.Identity;
-using HotelBookingPlatform.Core.Services.Contract;
-using Microsoft.AspNetCore.Identity;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Configuration;
 using Microsoft.IdentityModel.Tokens;
+using HotelBookingPlatform.Core.Entities.Identity;
+using HotelBookingPlatform.Core.Services.Contract;
 using System;
 using System.Collections.Generic;
 using System.IdentityModel.Tokens.Jwt;
@@ -9,40 +10,42 @@ using System.Linq;
 using System.Security.Claims;
 using System.Text;
 using System.Threading.Tasks;
-
 namespace HotelBookingPlatform.Application
 {
     public class TokenService : ITokenService
     {
+        private readonly IConfiguration Configuration;
+
+        public TokenService(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
         public async Task<string> CreateToken(AppUsers user, UserManager<AppUsers> userManager)
         {
-            // 1. Header  --> security algorithm (e.g., HS256) , type of token (JWT)
-            // 2. Payload --> 1. Registered claims (iss, sub, aud, exp, iat), 2. Private claims (custom data)
-            // 3. Signature (Key) --> HMACSHA256( base64UrlEncode(header) + "." + base64UrlEncode(payload), secretKey)  --> Install Package Authentication.JwtBearer
+            var authCalims = new List<Claim>
+        {
+            new Claim(ClaimTypes.Email, user.Email),
+            new Claim(ClaimTypes.GivenName, user.DisplayName)
+        };
 
-            var authClaims = new List<Claim>
+            var userRoles = await userManager.GetRolesAsync(user);
+            foreach (var role in userRoles)
             {
-                new Claim(ClaimTypes.Email, user.Email),
-                new Claim(ClaimTypes.GivenName, user.DisplayName),
-            };
-
-            var roles = await userManager.GetRolesAsync(user);
-            foreach (var role in roles)
-            {
-                authClaims.Add(new Claim(ClaimTypes.Role, role));
+                authCalims.Add(new Claim(ClaimTypes.Role, role));
             }
 
-            var authKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Environment.GetEnvironmentVariable("JWT_key")));
+            var authKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration["JWT:Key"]));
+
             var token = new JwtSecurityToken(
-            issuer: Environment.GetEnvironmentVariable("JWT_validIssuer"),
-            audience: Environment.GetEnvironmentVariable("JWT_validAudience"),
-            expires: DateTime.UtcNow.AddDays(7),
-            claims: authClaims,
-            signingCredentials: new SigningCredentials(authKey, SecurityAlgorithms.HmacSha256Signature)
+                issuer: Configuration["JWT:ValidIssuer"],
+                audience: Configuration["JWT:ValidAudience"],
+                expires: DateTime.UtcNow.AddDays(7),
+                claims: authCalims,
+                signingCredentials: new SigningCredentials(authKey, SecurityAlgorithms.HmacSha256Signature)
             );
 
             return new JwtSecurityTokenHandler().WriteToken(token);
-
         }
     }
 }
